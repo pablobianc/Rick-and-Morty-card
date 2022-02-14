0150ac8e083393b6b3fc6720e0ee6c19dfa93c08
pablo.bianco@hotmail.com <pablo.bianco@hotmail.com>
2022-02-14 17:05:43 -0300

    se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  };
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  };
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
+
+  return (
+    <>
+      <Navbar brand='Rick and Morty card' />
+
+      <div className='container mt-2'>
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+        <Characters characters={characters} />
+        <Pagination />
+      </div>
+    </>
+  );
+}
+
+export default App;
